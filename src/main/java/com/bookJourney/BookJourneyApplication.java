fe3f292cf7c1093433c75d817b4c5b32e1036222
Julia <julias@backbase.com>
2023-12-17 00:31:38 +0100

    First commit

---
+++ src/main/java/com/bookJourney/BookJourneyApplication.java
@@ -0,0 +1,13 @@
+package com.bookJourney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookJourneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookJourneyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bookJourney/BookJourneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookJourney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookJourneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
