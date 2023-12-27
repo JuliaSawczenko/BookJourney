c0a3685e705f0eb59b7ada0a77aab24b0297692a
Julia <julias@backbase.com>
2023-12-27 16:09:32 +0100

    Building structure

--- src/main/java/com/bookJourney/springboot/controller/UserController.java
+++ src/main/java/com/bookJourney/springboot/controller/UserController.java
@@ -1,7 +1,16 @@
 package com.bookJourney.springboot.controller;
 
+import com.bookJourney.springboot.dto.UserDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.Authenticator;
 
 @RestController
 public class UserController {
+
+    public ResponseEntity<?> register(Authentication authentication, UserDTO userDTO) {
+
+    }
 }
