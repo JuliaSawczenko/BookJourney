6063f7870af00907a588ee8d30dfa0e9c731334c
Julia <julias@backbase.com>
2023-12-31 01:55:35 +0100

    See profile logic

--- src/main/java/com/bookJourney/springboot/service/UserService.java
+++ src/main/java/com/bookJourney/springboot/service/UserService.java
@@ -16,7 +16,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
-import java.util.Optional;
 
 @Service
 public class UserService implements UserDetailsService {
