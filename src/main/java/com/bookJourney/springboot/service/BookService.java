47349a9f8c97956892ec3e07c0a317915d743392
julia <yulia.savchenko1@outlook.com>
2024-01-09 20:15:32 +0100

    Divide one method to several

--- src/main/java/com/bookJourney/springboot/service/BookService.java
+++ src/main/java/com/bookJourney/springboot/service/BookService.java
@@ -5,14 +5,14 @@ import com.bookJourney.springboot.config.BookNotFoundException;
 import com.bookJourney.springboot.dto.BookDTO;
 import com.bookJourney.springboot.entity.Book;
 import com.bookJourney.springboot.entity.BookDetail;
-import com.bookJourney.springboot.entity.Mood;
 import com.bookJourney.springboot.entity.User;
 import com.bookJourney.springboot.mapper.BookMapper;
 import com.bookJourney.springboot.repository.BookDetailRepository;
 import com.bookJourney.springboot.repository.BookRepository;
 import org.mapstruct.factory.Mappers;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class BookService {
@@ -35,54 +35,63 @@ public class BookService {
         this.bookDetailRepository = bookDetailRepository;
     }
 
+
     public void addBook(BookDTO bookDTO, String username) throws BookNotFoundException, BookAlreadyExistsException {
         User user = userService.getUserByUsername(username);
 
         if (checkIfBookExists(bookDTO.title(), bookDTO.author(), user)) {
             throw new BookAlreadyExistsException();
-        } else {
-            BookDetail bookDetail = bookDetailRepository.findByTitleAndAuthor(bookDTO.title(), bookDTO.author())
-                            .orElseGet(() -> {
-                                try {
-                                    return googleBooksService.getBookDetails(bookDTO.title(), bookDTO.author());
-                                } catch (BookNotFoundException e) {
-                                    throw new RuntimeException(e);
-                                }
-                            });
-
-            if (bookDetail.getId() == null) {
-                bookDetailRepository.save(bookDetail);
-            }
-
-            Book book = mapper.BookDTOtoBook(bookDTO);
-            book.setUser(user);
-            book.setBookDetail(bookDetail);
-            bookRepository.save(book);
-
-            switch (book.getStatus()) {
-                case READ:
-                    if (bookDTO.review() != null) {
-                        reviewService.addReview(bookDTO.review(), book, user);
-                    }
-                    if (bookDTO.moods() != null) {
-                        moodDataService.submitFinalMoods(bookDTO.moods());
-                    }
-                    break;
-                case READING:
-                    if (bookDTO.mood() != null) {
-                        moodDataService.addCurrentMood(bookDTO.mood());
-                    }
-                    break;
-                case GOING_TO_READ:
-                    if (bookDTO.startDate() != null) {
-                        book.setStartDate(bookDTO.startDate());
-                    }
-                    break;
-            }
         }
+
+        BookDetail bookDetail = fetchOrCreateBookDetail(bookDTO);
+
+        if (bookDetail.getId() == null) {
+            bookDetailRepository.save(bookDetail);
+        }
+        Book book = mapper.BookDTOtoBook(bookDTO);
+        book.setUser(user);
+        book.setBookDetail(bookDetail);
+        bookRepository.save(book);
+        handleBookStatus(bookDTO, user, book);
     }
+
 
     private boolean checkIfBookExists(String title, String author, User user) {
         return bookRepository.findByBookDetail_TitleAndBookDetail_AuthorAndUser(title, author, user).isPresent();
+    }
+
+
+    private BookDetail fetchOrCreateBookDetail(BookDTO bookDTO) throws BookNotFoundException {
+        Optional<BookDetail> bookDetailOptional = bookDetailRepository.findByTitleAndAuthor(bookDTO.title(), bookDTO.author());
+
+        if (bookDetailOptional.isPresent()) {
+            return bookDetailOptional.get();
+        } else {
+            return googleBooksService.getBookDetails(bookDTO.title(), bookDTO.author());
+        }
+    }
+
+
+    private void handleBookStatus(BookDTO bookDTO, User user, Book book) {
+        switch (book.getStatus()) {
+            case READ:
+                if (bookDTO.review() != null) {
+                    reviewService.addReview(bookDTO.review(), book, user);
+                }
+                if (bookDTO.moods() != null) {
+                    moodDataService.submitFinalMoods(bookDTO.moods());
+                }
+                break;
+            case READING:
+                if (bookDTO.mood() != null) {
+                    moodDataService.addCurrentMood(bookDTO.mood());
+                }
+                break;
+            case GOING_TO_READ:
+                if (bookDTO.startDate() != null) {
+                    book.setStartDate(bookDTO.startDate());
+                }
+                break;
+        }
     }
 }
