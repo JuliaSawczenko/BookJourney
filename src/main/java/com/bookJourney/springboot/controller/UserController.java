b645b204e0d5dfe615f1161c4006c3799b10ff2c
julia <yulia.savchenko1@outlook.com>
2024-01-11 00:19:05 +0100

    CORS

--- src/main/java/com/bookJourney/springboot/controller/UserController.java
+++ src/main/java/com/bookJourney/springboot/controller/UserController.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 import java.security.Principal;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/api")
 public class UserController {
 
