ee1fd06eb76866dee6180bc9aceed873f5c6551d
julia <yulia.savchenko1@outlook.com>
2024-01-11 01:18:43 +0100

    CORS

--- src/main/java/com/bookJourney/springboot/config/SecurityConfig.java
+++ src/main/java/com/bookJourney/springboot/config/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
                         .anyRequest().authenticated()
                 )
                 .httpBasic(Customizer.withDefaults())
-                .cors(cors -> cors.disable())
+                .cors(Customizer.withDefaults())
                 .csrf(configurer -> configurer.disable())
                 .build();
 
