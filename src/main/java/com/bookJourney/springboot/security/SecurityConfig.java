26a49ad590d32afc1a0570a4aab031d1f31645dc
julia <yulia.savchenko1@outlook.com>
2024-01-12 10:44:39 +0100

    JWT

--- src/main/java/com/bookJourney/springboot/security/SecurityConfig.java
+++ src/main/java/com/bookJourney/springboot/security/SecurityConfig.java
@@ -48,6 +48,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth ->
                         auth.requestMatchers("/auth/**").permitAll()
+                                .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**").permitAll()
                                 .anyRequest().authenticated()
                 );
 
