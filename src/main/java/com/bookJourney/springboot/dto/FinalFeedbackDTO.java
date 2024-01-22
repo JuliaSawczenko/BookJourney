2f4cad8d3d724019bc8afc50dec5be0bb2efa128
JuliaSawczenko <114311420+JuliaSawczenko@users.noreply.github.com>
2024-01-22 02:10:10 +0100

    Add logic for getting book details

--- src/main/java/com/bookJourney/springboot/dto/FinalFeedbackDTO.java
+++
@@ -1,9 +0,0 @@
-package com.bookJourney.springboot.dto;
-
-import com.bookJourney.springboot.entity.EnumMood;
-
-import java.util.HashMap;
-
-public record FinalFeedbackDTO(ReviewDTO review,
-                              HashMap<EnumMood, Integer> moods){
-}
