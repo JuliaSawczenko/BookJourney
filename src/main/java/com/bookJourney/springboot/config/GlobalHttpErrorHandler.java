896b1f25c0bc5ed8bef6930965ad6d5833f3399e
JuliaSawczenko <114311420+JuliaSawczenko@users.noreply.github.com>
2024-01-30 22:29:37 +0100

    Modifications

--- src/main/java/com/bookJourney/springboot/config/GlobalHttpErrorHandler.java
+++ src/main/java/com/bookJourney/springboot/config/GlobalHttpErrorHandler.java
@@ -1,5 +1,6 @@
 package com.bookJourney.springboot.config;
 
+import com.bookJourney.springboot.dto.MessageResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,17 +12,17 @@ public class GlobalHttpErrorHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(BookAlreadyExistsException.class)
     public ResponseEntity<Object> handleBookAlreadyExistsException(BookAlreadyExistsException exception) {
-        return new ResponseEntity<>("Book with a given title and author already exists in your library.", HttpStatus.BAD_REQUEST);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new MessageResponse("Book with a given title and author already exists in your library."));
     }
 
     @ExceptionHandler(BookNotFoundException.class)
     public ResponseEntity<Object> handleBookNotFoundException(BookNotFoundException exception) {
-        return new ResponseEntity<>("Not able to find the book.", HttpStatus.BAD_REQUEST);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new MessageResponse("Book not found."));
     }
 
     @ExceptionHandler(ReviewAlreadyExistsException.class)
     public ResponseEntity<Object> handleReviewAlreadyExistsException(ReviewAlreadyExistsException exception) {
-        return new ResponseEntity<>("Review for a given book already exists.", HttpStatus.BAD_REQUEST);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new MessageResponse("Review for a given book already exists"));
     }
 
 }
