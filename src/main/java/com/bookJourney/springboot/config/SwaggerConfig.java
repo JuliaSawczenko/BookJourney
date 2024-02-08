a7f61640077cfe704fc46f9ef0c8768b8f9ae011
JuliaSawczenko <114311420+JuliaSawczenko@users.noreply.github.com>
2024-02-08 01:16:23 +0100

    Swagger config

--- src/main/java/com/bookJourney/springboot/config/SwaggerConfig.java
+++ src/main/java/com/bookJourney/springboot/config/SwaggerConfig.java
@@ -11,7 +11,7 @@ public class SwaggerConfig {
         return GroupedOpenApi.builder()
                 .group("springshop-public")
                 .packagesToScan("com.bookJourney.springboot.controller")
-                .pathsToMatch("/auth/**", "/user/**", "/book/**", "/stats/**")
+                .pathsToMatch("/auth/**", "/user/**", "/book/**", "/stats/**", "/notifications/**")
                 .build();
     }
 }
--- src/main/java/com/bookJourney/springboot/controller/NotificationController.java
+++ src/main/java/com/bookJourney/springboot/controller/NotificationController.java
@@ -3,17 +3,19 @@ package com.bookJourney.springboot.controller;
 import com.bookJourney.springboot.service.NotificationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 
 @RestController
+@RequestMapping("/notifications")
 public class NotificationController {
 
     @Autowired
     private NotificationService notificationService;
 
-    @GetMapping("/notifications")
+    @GetMapping
     public SseEmitter subscribe() {
         return notificationService.createEmitter();
     }
